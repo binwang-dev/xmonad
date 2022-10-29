0200c286e12c073378855d4fc04549c4596d2467
Bin Wang <binwang.developer@outlook.com>
2022-10-29 09:28:22 -0400

    Update xmonad config

--- xmonad.hs
+++ xmonad.hs
@@ -1,11 +1,17 @@
 import XMonad
 
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
 
 main :: IO ()
-main = xmonad $ def
+main = xmonad $ ewmh def
   {
     terminal = "alacritty",
     modMask = mod4Mask
   }
+ `additionalKeysP`
+  [
+    ("M-p", spawn "rofi -show drun")
+  , ("M1-<Tab>", spawn "rofi -show window")
+  ]
