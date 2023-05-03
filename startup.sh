630d834380973e8bd8c018693b35e48e9b24dd23
Bin Wang <binjwang@users.noreply.github.com>
2023-05-03 17:47:35 -0400

    Update XMonad configuration and add startup script

---
+++ startup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+xrandr --output DP-2 --auto --pos 0x560 --primary
+xrandr --output DP-4 --auto --rotate left --pos 3440x0
+dunst &
--- xmonad.hs
+++ xmonad.hs
@@ -1,14 +1,23 @@
 import XMonad
 
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Layout.PerScreen
+import XMonad.Layout.Spacing
+import XMonad.Layout.ThreeColumns
 import XMonad.Util.EZConfig
+import XMonad.Util.SpawnOnce
 import XMonad.Util.Ungrab
+
+primaryLayout = smartSpacing 5 $ (Tall 1 (3/100) (618/1000) ||| Full ||| ThreeColMid 1 (3/100) (1/2))
+secondaryLayout = smartSpacing 5 (Mirror (Tall 1 (3/100) (1/2)) ||| Full)
 
 main :: IO ()
 main = xmonad $ ewmh def
   {
-    terminal = "alacritty",
-    modMask = mod4Mask
+    layoutHook = ifWider 2560 primaryLayout secondaryLayout,
+    modMask = mod4Mask,
+    startupHook = spawnOnce "~/.config/xmonad/startup.sh &",
+    terminal = "alacritty"
   }
  `additionalKeysP`
   [
