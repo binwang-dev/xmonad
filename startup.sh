d5650a5ef04fc1572979086bc95c10e50531e6db
Bin Wang <binjwang@users.noreply.github.com>
2023-05-03 21:46:19 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
-xrandr --output DP-2 --auto --pos 0x560 --primary
-xrandr --output DP-4 --auto --rotate left --pos 3440x0
+if [ $HOSTNAME = "cobra" ]; then
+	xrandr --output HDMI-1 --mode 2560x1080 --rate 75 --primary
+	xrandr --output HDMI-2 --mode 2560x1080 --rate 75 --above HDMI-1
+fi
+
+if [ $HOSTNAME = "mamba" ]; then
+	xrandr --output DP-2 --auto --pos 0x560 --primary
+	xrandr --output DP-4 --auto --rotate left --pos 3440x0
+fi
+
 dunst &
